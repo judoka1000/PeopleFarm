e5a7536cfaa48001a723929bcf2ac9fee6fd9bea
Marlies <marlies.hoekstra@duo.nl>
2017-10-19 14:30:34 +0200

    poging 2

---
+++ src/main/java/com/amazone/peoplefarm/PeoplefarmApplication.java
@@ -0,0 +1,12 @@
+package com.amazone.peoplefarm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeoplefarmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeoplefarmApplication.class, args);
+	}
+}
