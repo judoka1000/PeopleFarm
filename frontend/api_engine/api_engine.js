e8a12d076238dc435a1142cbac9ac45f2a8b0c97
maartenschipper <32929094+maartenschipper@users.noreply.github.com>
2017-10-30 13:54:55 +0100

    opgeruimd

--- frontend/api_engine/api_engine.js
+++ frontend/api_engine/api_engine.js
@@ -1,36 +1,68 @@
+
+
 app.factory('apiEngine', function apiEngine($http){
+    var mock = true;
+    var baseUrl = "http://home.maartenschipper.com:8002/PeopleFarmMock/";
 
     return {
         people: function(func){
-            console.log("Requesting /persons/");
-            return $http.get("http://home.maartenschipper.com:8002/PeopleFarmMock/status.jsp?task=getAll").then(func);
-            //return $http.get("http://home.maartenschipper.com:8002/PeopleFarmMock/status.jsp?task=getAll").then(func);
+            var functionPath = "/persons";
+            console.log("Requesting " + functionPath + mock ? " (mock)" : "");
+            if(mock) func(getMockData(functionPath));
+            else return $http.get(baseUrl + "status.jsp?task=getAll").then(func);
         },
-        status: function(id, func){
-            var url = "http://home.maartenschipper.com:8002/PeopleFarmMock/status.jsp?id=" + id + "";
-            console.log("Requesting /person/status/:" + id);
-            console.log("-> on " + url)
-            return $http.get(url).then(func);
+
+        personStatus: function(id,func){
+            var functionPath = "/person/status/:" + id;
+            console.log("Requesting " + functionPath);
+            if(mock) func(getMockData("/person/status"));
+            else return $http.get(baseUrl + "status.jsp?id=" + id).then(func);
+        },
+
+        personTask: function(id,func){
+            var functionPath = "/person/task/:" + id;
+            console.log("Requesting " + functionPath);
+            if(mock) func(getMockData("/person/task"));
+            else return $http.get(baseUrl + "status.jsp?task=getAll").then(func);
+        },
+
+        personSettask: function(id,task,func){
+            var functionPath = "/person/settask/:" + task + "/:+" + id;
+            console.log("Requesting " + functionPath);
+            if(mock) func(getMockData("/person/settask"));
+            else return $http.get(baseUrl + "status.jsp?task=getAll").then(func);
+        },
+
+        personSetTwoTask: function(id1, id2, task,func){
+            var functionPath = "/person/settask/:" + task + "/:+" + id1 + "/:" + id2;
+            console.log("Requesting " + functionPath);
+            if(mock) func(getMockData("/person/settask2"));
+            else return $http.get(baseUrl + "status.jsp?task=getAll").then(func);
+        },
+
+        ppeople: function(func){
+            console.log("Requesting /persons/");
+            return $http.get(baseUrl + "status.jsp?task=getAll").then(func);
         },
         updateStatus: function(id, func){
-            var url = "http://home.maartenschipper.com:8002/PeopleFarmMock/status.jsp?id=" + id + "&task=updatePerson";
+            var url = baseUrl + "status.jsp?id=" + id + "&task=updatePerson";
             console.log("Requesting /person/status/:" + id);
             console.log("-> on " + url)
             return $http.get(url).then(func);
         },
         task: function(id, func){
-            var url = "http://home.maartenschipper.com:8002/PeopleFarmMock/task.jsp?id=" + id;
+            var url = baseUrl + "task.jsp?id=" + id;
             console.log("Requesting /person/task/:" + id);
             console.log("-> on " + url)
             return $http.get(url).then(func);
         },
         reset: function(func){
-            var url = "http://home.maartenschipper.com:8002/PeopleFarmMock/status.jsp?task=reset";
+            var url = baseUrl + "status.jsp?task=reset";
             console.log("Requesting reset");
             return $http.get(url).then(func);
         },
         setTask: function(id, task, func){
-            var url = "http://home.maartenschipper.com:8002/PeopleFarmMock/status.jsp?id=" + id + "&task=" + task;
+            var url = baseUrl + "status.jsp?id=" + id + "&task=" + task;
             console.log("Requesting /person/settask/:" + task + "/" + id);
             console.log("-> on " + url)
             return $http.get(url).then(func);
---
+++ frontend/controller/controller.js
@@ -0,0 +1,111 @@
+var app = angular.module('PeopleApp', []);
+app.controller('PeopleCtrl', PeopleCtrl);
+
+function PeopleCtrl($scope,$http,$document,$interval,$timeout,apiEngine,personsFactory){
+    $scope.init = function(){
+        //$interval($scope.updateGamestate, 5000);
+    }
+
+    apiEngine.people( function (response) {
+        $scope.persons = personsFactory.addPersons(response.data.people);
+    });
+
+    $scope.cursor = "";
+    $scope.clickAction = "";
+
+    $scope.updateGamestate = function(){
+        console.log("updategame");
+        var persons = personsFactory.getPersons();
+
+        for (key in persons) {
+            persons[key].updateStatus();
+            console.log("person:");
+            console.log(persons[key]);
+        }
+    }
+
+    $scope.personClicked = function(person){
+        console.log("Person " + person.id + " clicked");
+        switch($scope.clickAction) {
+
+            case "eat":
+                console.log("Starting to eat, njam njam njam");
+                person.eat();
+            break;
+
+            case "sleep":
+                console.log("zzz zzz zzz");
+                person.sleep();
+                break;
+
+            case "kill":
+                console.log("Kill");
+                person.sprite = "Tombstone";
+                $timeout(function(){$scope.removePeople(person)},3500);
+                var audio = new Audio('sounds/screem.mp3');
+                audio.play();
+            break;
+
+            case "update":
+                console.log("reset");
+                person.update();
+            break;
+
+
+            default:
+                apiEngine.people(function (response) {
+                    //console.log(response.data.people);
+                    //$scope.people = response.data.people;
+                    tPeople = response.data.people;
+                    for (key of Object.keys(tPeople)) {
+                        for (key2 of Object.keys(tPeople[key])) {
+                            //console.log(key, tPeople[key]);
+                            $scope.people[key][key2] = tPeople[key][key2];
+                        }
+
+                    }
+                });
+        }
+
+    }
+
+    $scope.removePeople = function(person){
+        person.visible=false;
+    }
+
+    $scope.getBarPc = function(val,reverse=false,min=0,max=100){
+        var result = "";
+        var tOkeLow = max*0.33;
+        var tOkeHigh = max*0.66;
+        if(val > min & val <= tOkeLow){
+            result = reverse ? "good" : "danger";
+        }
+
+        if(val > tOkeLow & val <= tOkeHigh){
+            result = "oke";
+        }
+
+        if(val > tOkeHigh & val <= max){
+            result = reverse ? "danger" : "good";
+        }
+
+        return result;
+    }
+
+    $scope.setClickAction = function(action){
+        $scope.cursor = action + "Cur";
+        $scope.clickAction = action;
+        console.log("ready to " + action);
+    }
+
+    $scope.actionUpdate = function(){
+        apiEngine.reset(function(){});
+    }
+
+    $scope.actionNone = function(){
+        console.log("actionNone");
+        $scope.cursor = "";
+    }
+
+    $scope.init();
+}
